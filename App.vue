82ab6a9a4a50578a4ac2c29b8ff8e772608bec02
luckymichaln <michalnowak.contact@gmail.com>
2019-05-28 11:31:45 +0200

    project init

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">Vue</text>
+    <text class="lala">Yuhuuuu!</text>
+    </view>
+</template>
+
+<script>
+  export default { 
+  }
+</script>
+
+ 
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  background-color: lightgreen;
+}
+.text-color-primary {
+  color: black;
+  font-weight: 800;
+  margin-bottom: 20px;
+}
+
+.lala {
+  color: black;
+  font-weight: 800;
+}
+</style>
